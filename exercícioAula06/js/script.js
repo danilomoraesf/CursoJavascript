9f000f8e8b477413298c35f5bd94e12d179aa167
Danilo Moraes Ferreira <danilomoraesf@outlook.com>
2023-02-09 17:19:44 -0300

    Criação da página de produtos

---
+++ exercícioAula06/js/script.js
@@ -0,0 +1,34 @@
+
+
+function paginaProdutos(){
+    const header = document.createElement('header'); // criando header
+    const h1 = document.createElement('h1'); // criando header
+    h1.innerHTML = "Lista de Produtos"; // manipulando texto
+    header.appendChild(h1); // inserindo título no menu (header)
+    const main = document.createElement('main'); // onde ficarão as fotos
+   
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} 
+
+
+
+
+
+
+body.addEventListener("load", paginaProdutos());
