773ced1c74f64a96f1c2c137e244580748d8cf99
DANILO MORAES FERREIRA <danilo.mferreira6@senacsp.edu.br>
2023-02-23 17:39:07 -0300

    animações com funções

---
+++ aula10/js/anima2.js
@@ -0,0 +1,9 @@
+fundo.onclick = ()=>{
+    fundo.style.backgroundColor = "rgba(0,0,0,0.5)"
+    tela.style.top = "0px"
+}
+
+document.querySelector("#fechar").onclick = ()=>{
+    fundo.style.backgroundColor = "rgba(0,0,0,0)"
+    tela.style.top = "-600px"
+}
