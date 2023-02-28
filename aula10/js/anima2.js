ddcad9d90fe81c4269f603bbc77911a8e1462152
DANILO MORAES FERREIRA <danilo.mferreira6@senacsp.edu.br>
2023-02-28 17:30:44 -0300

    manipulação de dados na página, consulta e acesso de  API

--- aula10/js/anima2.js
+++ aula10/js/anima2.js
@@ -7,3 +7,11 @@ document.querySelector("#fechar").onclick = ()=>{
     fundo.style.backgroundColor = "rgba(0,0,0,0)"
     tela.style.top = "-600px"
 }
+
+fechar.onclick = ()=>{
+    console.log("fechou")
+    fundo.style.backgroundColor = "rgba(0,0,0,0)"
+    tela.style.top = "-600px"
+}
+
+console.log(window.screen.width)
